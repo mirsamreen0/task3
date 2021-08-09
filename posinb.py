3cefa971d17177cefbb856c1bda9b4436721b6b1
mirsamreen0 <88386505+mirsamreen0@users.noreply.github.com>
2021-08-09 22:02:01 +0530

    Add files via upload

---
+++ posinb.py
@@ -0,0 +1,12 @@
+istlist = (12,-7,5,64,-14)
+secondlist = (12,14,-95,3)
+
+for num in istlist :
+    if num >= 0:
+        print(num, end=" ")
+
+print('\n')
+
+for num in secondlist :
+    if num >= 0:
+        print(num, end=" ")
